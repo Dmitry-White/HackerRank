a485c680a887a35226a64460ad8dd0e390b44eea
Dmitry <ecio1476@gmail.com>
2017-07-09 02:07:13 +0800

    1st Day of Code (Python, Ruby)

---
+++ 30 Days of Code/Python/day_0.py
@@ -0,0 +1,12 @@
+"""
+Created on Sun Jul  9 01:55:17 2017
+
+@author: Dmitry White
+"""
+
+# TODO: Write a line of code here that prints the contents of input_string to stdout.
+
+input_string = input()
+
+print("Hello, World.")
+print(input_string)
---
+++ 30 Days of Code/Ruby/day_0.rb
@@ -0,0 +1,12 @@
+=begin
+Created on Sun Jul  9 01:55:17 2017
+
+@author: Dmitry White
+=end
+
+# TODO: Write a line of code here that prints the contents of input_string to stdout.
+
+input_string = gets
+
+puts 'Hello, World.'
+puts input_string
