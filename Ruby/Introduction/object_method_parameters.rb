568ba5d56ca632a11e6fcff3abe93115dbdb1177
Dmitry <ecio1476@gmail.com>
2017-07-10 15:21:04 +0800

    Solve Object Method Parameters

---
+++ Ruby/Introduction/object_method_parameters.rb
@@ -0,0 +1,13 @@
+=begin
+Created on Sun Jul  10 15:35:17 2017
+
+@author: Dmitry White
+=end
+
+=begin
+    Three variables a, b, and c are already defined.
+    Your task is to write code that checks whether a is within the range of b and c
+    by calling the method range? on a and passing b and c as arguments.
+=end
+
+a.range?(b,c)
