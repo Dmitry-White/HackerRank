f648816585f3cec89cf7a45e390afd39e0d8cdeb
Dmitry <ecio1476@gmail.com>
2017-07-10 14:53:03 +0800

    Solve Day 1

---
+++ 30 Days of Code/Ruby/day_1.rb
@@ -0,0 +1,19 @@
+=begin
+Created on Sun Jul  10 13:04:17 2017
+
+@author: Dmitry White
+=end 
+
+=begin
+ TODO: Print the sum of both integers on the first line,
+ the sum of both doubles (scaled to 1 decimal place) on the second line,
+ and then the two concatenated strings on the third line.
+=end
+
+i_2 = gets.to_i
+d_2 = gets.to_f
+s_2 = gets.to_s
+
+puts i + i_2
+puts d + d_2
+puts s + s_2
