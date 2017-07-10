ace67a7293da96d78c064dfb63460a1d231434c7
Dmitry <ecio1476@gmail.com>
2017-07-10 15:15:57 +0800

    Solve Python: Division

---
+++ Python/Introduction/python_division.py
@@ -0,0 +1,13 @@
+"""
+Created on Sun Jul  10 15:12:57 2017
+
+@author: Dmitry White
+"""
+
+# TODO: Read two integers and print two lines. The first line should contain integer division.
+# The second line should contain float division.
+
+a = int(input())
+b = int(input())
+print(a//b)
+print(a/b)
