7a9c194fbac3f7829e447f15776f4b3792f19290
Dmitry <ecio1476@gmail.com>
2017-07-10 13:08:51 +0800

    Solve Day 1

---
+++ 30 Days of Code/Python/day_1.py
@@ -0,0 +1,17 @@
+"""
+Created on Sun Jul  10 13:04:17 2017
+
+@author: Dmitry White
+"""
+
+# TODO: Print the sum of both integers on the first line,
+# the sum of both doubles (scaled to 1 decimal place) on the second line,
+# and then the two concatenated strings on the third line.
+
+i_2 = int(input())
+d_2 = float(input())
+s_2 = input()
+
+print(i + i_2)
+print(d + d_2)
+print(s + s_2)
