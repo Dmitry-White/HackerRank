5f32aeebc3bd4a59183f2ff35fd4c59e019c5164
Dmitry <ecio1476@gmail.com>
2017-07-11 14:19:40 +0800

    Solve Loops

---
+++ Python/Introduction/loops.py
@@ -0,0 +1,11 @@
+"""
+Created on Sun Jul  11 14:15:57 2017
+
+@author: Dmitry White
+"""
+
+# TODO: Read an integer N. For all non-negative integers i<N, print i^2.
+
+n = int(input())
+for i in range(n):
+    print(i**2)
