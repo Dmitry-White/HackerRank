2c5e57a15a9c47d062c3f14577ffc2efba61f40e
Dmitry <ecio1476@gmail.com>
2017-07-12 23:16:52 +0800

    Refactor 'Write a Function'

--- Python/Introduction/write_a_function.py
+++ Python/Introduction/write_a_function.py
@@ -6,17 +6,17 @@ Created on Sun Jul  11 14:23:57 2017
 
 # TODO: You are given the year, and you have to write a function to check if the year is leap or not.
 
-def is_leap(year):
-    leap = False
-    
-    if year % 100 == 0:
-        if year % 400 == 0:
-            leap = True
-    else:
-        if year % 4 == 0:
-            leap = True
-    
-    return leap
+#def is_leap(year):
+#    leap = False
+#    
+#    if year % 100 == 0:
+#        if year % 400 == 0:
+#            leap = True
+#    else:
+#        if year % 4 == 0:
+#            leap = True
+#    
+#    return leap
 
 def is_leap(year):
     return year % 4 == 0 and (year % 400 == 0 or year % 100 != 0)
