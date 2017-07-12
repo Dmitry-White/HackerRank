dc110b24526f241231b97d1843d5fc44562807a6
Dmitry <ecio1476@gmail.com>
2017-07-12 23:21:09 +0800

    Refactor 'Print Function'

--- Python/Introduction/print_function.py
+++ Python/Introduction/print_function.py
@@ -8,16 +8,24 @@ Created on Sun Jul  11 14:32:57 2017
 # Without using any string methods, try to print the following:
 # 123...N ("..." represents the values in between)
 
-n = int(input())
-s = []
-result = ""
-for i in range(1,n+1):
-    s.append(str(i))
-print(''.join(s))
+#------------1st----------------
 
-N = int(raw_input())
-for i in range(1, N+1):
-    print(i, end="")
+#n = int(input())
+#s = []
+#result = ""
+#for i in range(1,n+1):
+#    s.append(str(i))
+#print(''.join(s))
+
+#-------------------------------
+
+#-----------2nd-----------------
+
+#N = int(input())
+#for i in range(1, N+1):
+#    print(i, end="")
+
+#-------------------------------
     
 print(*range(1, int(input())+1), sep='')
 
