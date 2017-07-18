038be7b4d9f8a64944e4d06ac76bcdf8e7ef3227
Dmitry <ecio1476@gmail.com>
2017-07-18 23:43:03 +0300

    Solve Tuples

---
+++ Python/Basic Data Types/tuples.py
@@ -0,0 +1,14 @@
+"""
+Created on Sun Jul  18 23:40:57 2017
+
+@author: Dmitry White
+"""
+
+# TODO: Given an integer, n, and n space-separated integers as input,
+# create a tuple, t, of those n integers.
+# Then compute and print the result of hash(t).
+
+n = int(input())
+integer_list = list(map(int, input().split()))
+print(hash(tuple(integer_list)))
+
