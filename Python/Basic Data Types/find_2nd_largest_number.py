11e1283a89a269e7ec1f836be51b3b924f99bebe
Dmitry <ecio1476@gmail.com>
2017-07-20 23:56:08 +0300

    Solve Find the Second Largest Number

---
+++ Python/Basic Data Types/find_2nd_largest_number.py
@@ -0,0 +1,13 @@
+"""
+Created on Sun Jul  20 23:40:57 2017
+
+@author: Dmitry White
+"""
+
+# TODO: You are given N numbers. 
+# Store them in a list and find the second largest number.
+
+n = int(input())
+arr = list(set(map(int, input().split())))
+arr.remove(max(arr))
+print(max(arr))
