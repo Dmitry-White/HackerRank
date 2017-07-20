29ec8d28b8bf458bccaf439ce1f6a6abd99832e3
Dmitry <ecio1476@gmail.com>
2017-07-20 23:46:49 +0300

    Solve Until

---
+++ Ruby/Control Structures/until.rb
@@ -0,0 +1,12 @@
+=begin
+Created on Sun Jul  20 23:47:17 2017
+
+@author: Dmitry White
+=end
+
+=begin 
+    TODO: Call the method coder.practice until coder.oh_one? becomes true.
+    Use the until control structure
+=end 
+
+coder.practice until coder.oh_one?
