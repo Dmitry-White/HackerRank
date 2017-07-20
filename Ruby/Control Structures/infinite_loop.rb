cbbc289cf482e89491daf81381bff32b0ccf6790
Dmitry <ecio1476@gmail.com>
2017-07-20 23:44:22 +0300

    Solve Infinite Loop

---
+++ Ruby/Control Structures/infinite_loop.rb
@@ -0,0 +1,16 @@
+=begin
+Created on Sun Jul  20 00:17:17 2017
+
+@author: Dmitry White
+=end
+
+=begin 
+    TODO: Use an infinite loop and call the method coder.practice within it and break if coder.oh_one? is true.
+=end 
+
+loop do
+    coder.practice
+    if coder.oh_one?
+        break
+    end
+end
