dbc19d5b90a85494d463c6afcf9a71b27d0654ae
Dmitry <ecio1476@gmail.com>
2017-07-21 23:58:02 +0300

    Solve Initialization

---
+++ Ruby/Array & Hashes/initialization.rb
@@ -0,0 +1,16 @@
+=begin
+Created on Fri Jul  20 23:54:27 2017
+
+@author: Dmitry White
+=end
+
+=begin 
+    TODO: Your task is to initialize three different variables:
+        Initialize an empty array with the variable name array
+        Initialize an array with exactly one nil element in it with the variable name array_1
+        Initialize an array with exactly two elements with value 10 in it using the variable name array_2.
+=end 
+
+array = Array.new
+array_1 = Array.new(1)
+array_2 = Array.new(2,10)
