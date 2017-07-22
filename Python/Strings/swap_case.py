83c39f12f31d0b23722fc62f705b2c00f207ae82
Dmitry <ecio1476@gmail.com>
2017-07-22 20:48:25 +0300

    Solve sWAP cASE

---
+++ Python/Strings/swap_case.py
@@ -0,0 +1,10 @@
+"""
+Created on Sat Jul  22 20:45:01 2017
+
+@author: Dmitry White
+"""
+
+# TODO: You are given a string S. Your task is to swap cases.
+
+def swap_case(s):
+    return  s.swapcase()
