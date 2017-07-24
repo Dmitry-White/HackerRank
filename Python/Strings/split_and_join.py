d045be052fc50b1b9094ad5b3f6132073cae51ef
Dmitry <ecio1476@gmail.com>
2017-07-24 23:22:02 +0300

    Solve Split and Join

---
+++ Python/Strings/split_and_join.py
@@ -0,0 +1,10 @@
+"""
+Created on Sat Jul  24 23:02:21 2017
+
+@author: Dmitry White
+"""
+
+# TODO: You are given a string. Split the string on a " " (space) delimiter and join using a - hyphen.
+
+def split_and_join(line):
+    return "-".join(line.split(" "))
