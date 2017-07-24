41dad57063912f20cbb696f509878c46d9823a6c
Dmitry <ecio1476@gmail.com>
2017-07-24 23:33:24 +0300

    Solve What's Your Name?

---
+++ Python/Strings/whats_your_name.py
@@ -0,0 +1,13 @@
+"""
+Created on Mon Jul  24 23:11:01 2017
+
+@author: Dmitry White
+"""
+
+# TODO: You are given the firstname and lastname of a person on two different lines.
+# Your task is to read them and print the following:
+#   Hello firstname lastname! You just delved into python.
+
+def print_full_name(a, b):
+    print("Hello", a, b, end="")
+    print("! You just delved into python.")
