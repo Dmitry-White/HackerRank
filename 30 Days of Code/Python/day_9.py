e7ca5c623558bbd340e5f42f09863ced2719f394
Dmitry <ecio1476@gmail.com>
2017-07-28 04:52:22 +0300

    Solve Dat 9

---
+++ 30 Days of Code/Python/day_9.py
@@ -0,0 +1,16 @@
+"""
+Created on Wed Jul  26 16:25:11 2017
+
+@author: Dmitry White
+"""
+
+# TODO: Write a factorial function that takes a positive integer, N,
+#  as a parameter and prints the result of N!(N factorial).
+
+def factorial(n):
+    if n == 1:
+        return 1
+    else:
+        return n * factorial(n-1)
+        
+print(factorial(int(input())))
