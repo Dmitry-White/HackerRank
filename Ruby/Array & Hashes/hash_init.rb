0937ee7604f7290fc3c7363d3d2b3a4942ead0b3
Dmitry <ecio1476@gmail.com>
2017-07-29 14:31:07 +0300

    Solve Hash: Initialization

---
+++ Ruby/Array & Hashes/hash_init.rb
@@ -0,0 +1,16 @@
+=begin
+Created on Fri Jul  28 14:28:41 2017
+
+@author: Dmitry White
+=end
+
+=begin
+    TODO: create three different Hash collections:
+        Initialize an empty Hash with the variable name empty_hash
+        Initialize an empty Hash with the variable name default_hash and the default value of every key set to 1
+        Initialize a hash with the variable name hackerrank and having the key-value pairs
+=end
+
+empty_hash = Hash.new
+default_hash = Hash.new(1)
+hackerrank = {"simmy" => 100, "vivmbbs" => 200}
