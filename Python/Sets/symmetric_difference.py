a7f5a3b3e11682cbc4b1c9580f8abbfc82e5dce7
Dmitry White <dmitry.b.white@gmail.com>
2017-08-02 23:56:46 +0300

    Solve Symmetric Difference

---
+++ Python/Sets/symmetric_difference.py
@@ -0,0 +1,18 @@
+"""
+Created on  Aug  02 23:13:51 2017
+
+@author: Dmitry White
+"""
+
+# TODO: Given 2 sets of integers, M and N,
+# print their symmetric difference in ascending order.
+#  The term symmetric difference indicates those values that
+#  exist in either M or N but do not exist in both.
+
+input()
+a = set(input().split())
+input()
+b = set(input().split())
+
+print(*sorted(map(int,(a.difference(b)  ^  b.difference(a)))), sep='\n')
+
