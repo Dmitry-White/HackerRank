56807694da26bb6508e059fedc74eb6426044306
Dmitry White <dmitry.b.white@gmail.com>
2017-08-07 23:43:50 +0300

    Solve Intersections

---
+++ Python/Sets/set_intersection.py
@@ -0,0 +1,17 @@
+"""
+Created on Mon Aug 07 23:40:33 2017
+
+@author: Dmitry White
+"""
+
+# TODO: The students of District College have subscriptions to English and French newspapers.
+#  Some students have subscribed only to English, some have subscribed only to French,
+#  and some have subscribed to both newspapers. You are given two sets of student roll numbers.
+#  One set has subscribed to the English newspaper, one set has subscribed to the French newspaper.
+#  Your task is to find the total number of students who have subscribed to both newspapers.
+
+input()
+eng = set(map(int, input().split()))
+input()
+fre = set(map(int, input().split()))
+print(len(eng & fre))
