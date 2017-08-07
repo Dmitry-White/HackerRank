8dca44503f8e90581e86d640cb10da5861e80781
Dmitry White <dmitry.b.white@gmail.com>
2017-08-07 23:44:38 +0300

    Correct Task Description

--- 30 Days of Code/Ruby/day_3.rb
+++ 30 Days of Code/Ruby/day_3.rb
@@ -4,7 +4,11 @@ Created on Mon Aug 07 23:35:22 2017
 @author: Dmitry White
 =end
 
-# TODO: Write a line of code here that prints the contents of input_string to stdout.
+# TODO: Given an integer, n, perform the following conditional actions:
+# If n is odd, print Weird
+# If n is even and in the inclusive range of 2 to 5, print Not Weird
+# If n is even and in the inclusive range of 6 to 20, print Weird
+# If n is even and greater than 20, print Not Weird
 
 N = gets.strip.to_i
 
