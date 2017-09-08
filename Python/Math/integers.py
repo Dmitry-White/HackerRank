b79d844638cac3eb8ff760d3a3ec97e199fd8ad0
Dmitry White <dmitry.b.white@gmail.com>
2017-09-08 13:06:30 +0200

    Solve Integers

---
+++ Python/Math/integers.py
@@ -0,0 +1,11 @@
+"""
+Created on Fri Sep 08 13:04:21 2017
+
+@author: Dmitry White
+"""
+
+# TODO: Read four numbers, a, b, c, and d, and print the result of a^b + c^d.
+
+a, b, c, d = int(input()), int(input()), int(input()), int(input())
+print(pow(a, b)+pow(c, d))
+
