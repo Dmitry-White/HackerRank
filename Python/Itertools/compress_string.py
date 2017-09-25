ac4d28ed11f99b86a17718aa726fc073d8bb29f6
Dmitry White <dmitry.b.white@gmail.com>
2017-09-26 01:03:35 +0300

    Solve Compress a String

---
+++ Python/Itertools/compress_string.py
@@ -0,0 +1,13 @@
+"""
+Created on Mon Sep 26 00:59:41 2017
+@author: Dmitry White
+"""
+
+# TODO: You are given a string S. Suppose a character 'c' occurs
+# consecutively X times in the string.
+# Replace these consecutive occurrences of the character 'c'
+# with (X,c) in the string.
+
+import itertools
+s = input()
+print(*[(len(list(g)), int(k)) for k, g in itertools.groupby(s)])
