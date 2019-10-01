795318d6630eb93ec61d3cdf2e3c043f8619c4f7
Dzmitry Belaichuk <dzmitry_belaichuk@epam.com>
2019-10-01 18:16:04 +0300

    Start JS journey

---
+++ 30 Days of Code/JavaScript/day_0.js
@@ -0,0 +1,25 @@
+/*
+Created on Tue Oct 1 04:23 2019
+
+@author: Dmitry White
+*/
+
+// TODO: Write a line of code here that prints the contents of input to stdout.
+
+function processData(inputString) {
+  console.log("Hello, World.");
+  console.log(inputString);
+}
+
+
+// Service code
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+_input = "";
+process.stdin.on("data", function (input) {
+    _input += input;
+});
+
+process.stdin.on("end", function () {
+   processData(_input);
+});
