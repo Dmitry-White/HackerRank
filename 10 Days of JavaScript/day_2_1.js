6acd3a760a38e283342e8ae9297ba1bd3bbb3265
Dzmitry Belaichuk <dzmitry_belaichuk@epam.com>
2019-10-02 15:37:29 +0300

    Solve JS Day 2

---
+++ 10 Days of JavaScript/day_2_1.js
@@ -0,0 +1,57 @@
+'use strict';
+
+function getLetter(s) {
+    const letter = s[0];
+
+    const subsetA = {
+        string: 'aeiou',
+        value: 'A',
+    };
+    const subsetB = {
+        string: 'bcdfg',
+        value: 'B',
+    };
+    const subsetC = {
+        string: 'hjklm',
+        value: 'C',
+    };
+    const subsetD = {
+        string: 'npqrstvwxyz',
+        value: 'D',
+    };
+    
+    switch (true) {
+        case subsetA.string.includes(letter):
+            return subsetA.value;
+        case subsetB.string.includes(letter):
+            return subsetB.value;
+        case subsetC.string.includes(letter):
+            return subsetC.value;
+        case subsetD.string.includes(letter):
+            return subsetD.value;
+    }
+}
+
+// Service Code
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
---
+++ 10 Days of JavaScript/day_2_2.js
@@ -0,0 +1,40 @@
+'use strict';
+
+/*
+ * Complete the vowelsAndConsonants function.
+ * Print your output using 'console.log()'.
+ */
+function vowelsAndConsonants(s) {
+    const vowelsAlphabet = 'aeiou';
+    const vowels = [];
+    const consonants = [];
+    for (let i = 0; i <= s.length; i++) {
+        vowelsAlphabet.includes(s[i])
+            ? vowels.push(s[i])
+            : consonants.push(s[i]);
+    }
+    console.log(vowels.join("\n") + "\n" + consonants.join("\n"))
+}
+
+// Service Code
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
