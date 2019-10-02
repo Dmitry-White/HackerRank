80d375b353b8ef6c27c8e1e7e57167d8652abb96
Dzmitry Belaichuk <dzmitry_belaichuk@epam.com>
2019-10-02 16:23:43 +0300

    Solve JS Day 3

---
+++ 10 Days of JavaScript/day_3_0.js
@@ -0,0 +1,44 @@
+'use strict';
+
+/**
+*   Return the second largest number in the array.
+*   @param {Number[]} nums - An array of numbers.
+*   @return {Number} The second largest number in the array.
+**/
+function getSecondLargest(nums) {
+    let max = 0;
+    let prev_max = 0;
+
+    nums.sort((a, b) => a - b).forEach(num => {
+        if (num > max) {
+            prev_max = max;
+            max = num;
+        }
+    });
+
+    return prev_max;
+}
+
+// Service Code
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
