94af7bb2a17e0467429f6809207e8bfc152ea621
Dzmitry Belaichuk <dzmitry_belaichuk@epam.com>
2019-10-02 14:14:33 +0300

    Solve JS Day 1

---
+++ 10 Days of JavaScript/day_1_1.js
@@ -0,0 +1,41 @@
+'use strict';
+
+/*
+ * Create the function factorial here
+ */
+const factorial = n => {
+  if (n <= 0) {
+    return 1
+  }
+  return n * factorial(n-1);
+}
+
+
+// Service Code
+function main() {
+    const n = +(readLine());
+    
+    console.log(factorial(n));
+}
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
