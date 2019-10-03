b3d6335d3dd27f60192613f2cd5bd80ce973f7b3
Dzmitry Belaichuk <dzmitry_belaichuk@epam.com>
2019-10-03 18:13:31 +0300

    Start JS Day 7

---
+++ 10 Days of JavaScript/day_7_1.js
@@ -0,0 +1,46 @@
+'use strict';
+
+function regexVar() {
+    /*
+     * Declare a RegExp object variable named 're'
+     * It must match a string that starts with 'Mr.', 'Mrs.', 'Ms.', 'Dr.', or 'Er.', 
+     * followed by one or more letters.
+     */
+    
+    const re = /\b^(Mr\.|Mrs\.|Ms\.|Dr\.|Er\.)[a-zA-Z]+$\b/
+    /*
+     * Do not remove the return statement
+     */
+    return re;
+}
+
+
+// Service Code
+function main() {
+    const re = regexVar();
+    const s = readLine();
+    
+    console.log(!!s.match(re));
+}
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
